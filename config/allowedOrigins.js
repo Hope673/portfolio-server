3d363c35c35fa4cb76f2e22833d575fd479de7c7
Hope673 <opatoro52@gmail.com>
2026-09-19 19:31:49 +0100

    made changes to corsOptions

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'https://adeyemiportfolio-nu.vercel.app',
+    'https://portfolio-client-two-beige.vercel.app/'
 ]
 module.exports = allowedOrigins
