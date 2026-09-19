a280eaccecdc2f459594ace091d01ef6bf2e3ebd
Hope673 <opatoro52@gmail.com>
2026-09-19 20:08:28 +0100

    change url 2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://adeyemiportfolio-nu.vercel.app',
     'https://portfolio-client-two-beige.vercel.app/'
 ]
 module.exports = allowedOrigins
