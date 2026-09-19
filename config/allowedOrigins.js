1efe1e30126221eea64e76498f3cb1e3aad9cc91
Hope673 <opatoro52@gmail.com>
2026-09-19 20:27:08 +0100

    v3

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://portfolio-client-two-beige.vercel.app/'
+    'https://portfolio-client-two-beige.vercel.app'
 ]
 module.exports = allowedOrigins
