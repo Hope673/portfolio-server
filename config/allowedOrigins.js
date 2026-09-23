b5f1651f9748258ac50820316c1395232c80cd80
Hope673 <opatorohope5@gmail.com>
2026-09-23 18:34:34 +0100

    changed allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://adeyemis-portfolio.onrender.com',
+    'https://adeyemi-portfolio-beige.vercel.app',
 ]
 module.exports = allowedOrigins
